53f0fa70c41412b26256fb93b120561d7ca46f77
guilleHuidobro <ghxghxgh@gmail.com>
2017-05-18 15:19:19 -0300

    add icons to fabtoolbar

--- app/src/main/java/com/plateado/pregol/ghuidobro/pregol/PGPagoActivity.java
+++ app/src/main/java/com/plateado/pregol/ghuidobro/pregol/PGPagoActivity.java
@@ -1,15 +1,25 @@
 package com.plateado.pregol.ghuidobro.pregol;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
 
 import com.daimajia.swipe.util.Attributes;
+import com.github.fafaldo.fabtoolbar.widget.FABToolbarLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,6 +32,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by ghuidobro on 4/12/17.
@@ -30,18 +41,35 @@ public class PGPagoActivity extends AppCompatActivity {
 
     private ArrayList<PGFixture> mDataSet;
     private RecyclerView mRecyclerView;
-    HashMap<String, String> prediccion ;
-    PagoViewAdapter mAdapter;
-    String usuario;
-    ArrayList<ItemPrediction> itemPredictions = new ArrayList<>();
+    private HashMap<String, String> prediccion ;
+    private PagoViewAdapter mAdapter;
+    private String usuario;
+    private ArrayList<ItemPrediction> itemPredictions = new ArrayList<>();
+    private FABToolbarLayout morph;
+    private FloatingActionButton fab;
+    private String sendMessage = "La prediccion no esta completa !";
+    private boolean isOKToSend = false;
+    private ProgressDialog loading;
+    private Handler handler;
+
+    View uno, dos, sendIcon, cuatro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pagorecyclerview);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        morph = (FABToolbarLayout) findViewById(R.id.fabtoolbar);
+        sendIcon = findViewById(R.id.send_icon);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        uno = findViewById(R.id.uno);
+        dos = findViewById(R.id.dos);
+        cuatro = findViewById(R.id.cuatro);
 
         usuario = getIntent().getStringExtra("mail");
+
+        handler = new Handler();
 
         // Layout Managers:
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -74,16 +102,72 @@ public class PGPagoActivity extends AppCompatActivity {
                 super.onScrolled(recyclerView, dx, dy);
             }
         });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view.getId() == R.id.fab) {
+                    morph.show();
+                }
+                morph.hide();
+            }
+        });
+        uno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                morph.hide();
+            }
+        });
+        dos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                morph.hide();
+            }
+        });
+        sendIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData();
+                if(isOKToSend){
+                    sendMessage = "Se envio con exito!";
+                }
+                morph.hide();
+
+                Snackbar snack = Snackbar.make(view, sendMessage, Snackbar.LENGTH_LONG);
+                View snackbarView = snack.getView();
+                snackbarView.setBackgroundColor(Color.parseColor("#a92b00"));
+                snackbarView.setMinimumHeight(210);
+                TextView tv = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                tv.setTextColor(ContextCompat.getColor(PGPagoActivity.this, R.color.window_background));
+                tv.setTextSize(20);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                }else {
+                    tv.setGravity(Gravity.CENTER_HORIZONTAL);
+                }
+                snack.show();
+            }
+        });
+        cuatro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                morph.hide();
+            }
+        });
     }
 
 
     public void onResume(){
         super.onResume();
-
+        if(loading != null){
+            loading.dismiss();
+        }
         if (mDataSet.isEmpty()) {
             mRecyclerView.setVisibility(View.GONE);
+            morph.setVisibility(View.GONE);
         } else {
             mRecyclerView.setVisibility(View.VISIBLE);
+            morph.setVisibility(View.VISIBLE);
         }
     }
 
@@ -186,9 +270,11 @@ public class PGPagoActivity extends AppCompatActivity {
             }//end of for loop
             if (mDataSet.isEmpty()) {
                 mRecyclerView.setVisibility(View.GONE);
+                morph.setVisibility(View.GONE);
 
             } else {
                 mRecyclerView.setVisibility(View.VISIBLE);
+                morph.setVisibility(View.VISIBLE);
             }
 
         }catch(JSONException e){
@@ -197,6 +283,73 @@ public class PGPagoActivity extends AppCompatActivity {
         }//end of catch
 
     }//end of convertJSONArrayList
+
+    private void sendData(){
+
+        if(mAdapter.prediccion.size() == 15){
+            prediccion = (HashMap<String, String>) mAdapter.prediccion;
+            prediccion.put("fecha","19");
+            prediccion.put("usuario",usuario);
+            isOKToSend = true;
+
+            for (Map.Entry<String, String> entry : prediccion.entrySet()) {
+                System.out.println("clave=" + entry.getKey() + ", valor=" + entry.getValue());
+            }
+
+
+            class AddFixture extends AsyncTask<Void,Void,String> {
+
+
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    new Thread(new PGPagoActivity.Task()).start();
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    if(loading != null){
+                        loading.dismiss();
+                    }
+                    Intent intent = new Intent(PGPagoActivity.this, PGPagoActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                protected String doInBackground(Void... v) {
+                    RequestHandler rh = new RequestHandler();
+                    String res = rh.sendPostRequest(Config.URL_ADD, prediccion);
+                    return res;
+                }
+            }
+
+            AddFixture addFixture = new AddFixture();
+            addFixture.execute();
+
+        }else{
+            isOKToSend = false;
+        }
+
+    }//end send data
+
+    private class Task implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    loading = ProgressDialog.show(PGPagoActivity.this, "Enviando...", "Un momento...", false, false);
+                }
+            });
+        }
+    }
 
     @Override
     public void onBackPressed() {
