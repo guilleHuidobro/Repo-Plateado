9bf57f95a99ab31a0a46a340c525b5bf4fdbc1df
guilleHuidobro <ghxghxgh@gmail.com>
2017-06-01 11:28:41 -0300

    changes

--- app/src/main/java/com/plateado/pregol/ghuidobro/pregol/SessionManager.java
+++ app/src/main/java/com/plateado/pregol/ghuidobro/pregol/SessionManager.java
@@ -106,16 +106,6 @@ public class SessionManager {
         editor.clear();
         editor.commit();
 
-        // After logout redirect user to Loing Activity
-        Intent i = new Intent(_context, MGMainActivity.class);
-        // Closing all the Activities
-        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-        // Add new Flag to start new Activity
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        // Staring Login Activity
-        _context.startActivity(i);
     }
 
     /**
